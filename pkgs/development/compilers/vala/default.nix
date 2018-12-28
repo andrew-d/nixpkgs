f48f9087afecbb5fe201b49daa464ff080ec93f2
Andrew Dunham <andrew@du.nham.ca>
2018-12-28 02:11:09 -0500

    vala_0_38, vala_0_40, vala_0_42: add configuration to disable graphviz
    This allows building Vala without support for Graphviz; useful for more
    minimal installs where we don't want to pull it (and transitively,
    pango, gd, etc.) in as a dependency.
    
    NOTE: this is a slightly modified version of the upstream PR I submitted
    that uses `if/else` instead of `lib.optional[s]`, to avoid having to
    rebuild everything that depends on vala.

--- pkgs/development/compilers/vala/default.nix
+++ pkgs/development/compilers/vala/default.nix
@@ -1,11 +1,45 @@
-{ stdenv, lib, fetchurl, pkgconfig, flex, bison, libxslt, autoconf, automake, graphviz
-, glib, libiconv, libintl, libtool, expat
+{ stdenv, lib, fetchurl, fetchpatch, pkgconfig, flex, bison, libxslt, autoconf, automake, autoreconfHook
+, graphviz, glib, libiconv, libintl, libtool, expat
 }:
 
 let
-  generic = { major, minor, sha256, extraNativeBuildInputs ? [], extraBuildInputs ? [] }:
+  generic = lib.makeOverridable ({
+    major, minor, sha256,
+    extraNativeBuildInputs ? [],
+    extraBuildInputs ? [],
+    withGraphviz ? true
+  }:
   let
     atLeast = lib.versionAtLeast "${major}.${minor}";
+
+    # Patches from the openembedded-core project to build vala without graphviz
+    # support. We need to apply an additional patch to allow building when the
+    # header file isn't available at all, but that patch (./gvc-compat.patch)
+    # can be shared between all versions of Vala so far.
+    graphvizPatch =
+      let
+        fp = { commit, sha256 }: fetchpatch {
+          url = "https://github.com/openembedded/openembedded-core/raw/${commit}/meta/recipes-devtools/vala/vala/disable-graphviz.patch";
+          inherit sha256;
+        };
+
+      in {
+        "0.38" = fp {
+          commit = "2c290f7253bba5ceb0d32e7d0b0ec0d0e81cc263";
+          sha256 = "056ybapfx18d7xw1k8k85nsjrc26qk2q2d9v9nz2zrcwbq5brhkp";
+        };
+        "0.40" = fp {
+          commit = "dfbbff39cfd413510abbd60930232a9c6b35d765";
+          sha256 = "19iyrljirpz63wi539bjnd06nq62dih2la3j93ckkv44jlc110s2";
+        };
+        "0.42" = fp {
+          commit = "8553c52f174af4c8c433c543f806f5ed5c1ec48c";
+          sha256 = "0qhvp3846l9jkkl894kcmriyzzfp66qwy5hcngagg6hg061wr8al";
+        };
+      }.${major} or (throw "no graphviz patch for this version of vala");
+
+    disableGraphviz = atLeast "0.38" && !withGraphviz;
+
   in stdenv.mkDerivation rec {
     name = "vala-${version}";
     version = "${major}.${minor}";
@@ -19,16 +53,24 @@ let
       patchShebangs tests
     '';
 
+    # If we're disabling graphviz, apply the patches and corresponding
+    # configure flag. We also need to override the path to the valac compiler
+    # so that it can be used to regenerate documentation.
+    patches        = if disableGraphviz then [ graphvizPatch ./gvc-compat.patch ] else null;
+    configureFlags = if disableGraphviz then "--disable-graphviz" else null;
+    preBuild       = if disableGraphviz then "buildFlagsArray+=(\"VALAC=$(pwd)/compiler/valac\")" else null;
+
     outputs = [ "out" "devdoc" ];
 
     nativeBuildInputs = [
       pkgconfig flex bison libxslt
     ] ++ lib.optional (stdenv.isDarwin && (atLeast "0.38")) expat
+      ++ lib.optional disableGraphviz autoreconfHook # if we changed our ./configure script, need to reconfigure
       ++ extraNativeBuildInputs;
 
     buildInputs = [
       glib libiconv libintl
-    ] ++ lib.optional (atLeast "0.38") graphviz
+    ] ++ lib.optional (atLeast "0.38" && withGraphviz) graphviz
       ++ extraBuildInputs;
 
     doCheck = false; # fails, requires dbus daemon
@@ -40,7 +82,7 @@ let
       platforms = platforms.unix;
       maintainers = with maintainers; [ antono jtojnar lethalman peterhoeg ];
     };
-  };
+  });
 
 in rec {
   vala_0_34 = generic {
