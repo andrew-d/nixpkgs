2e01c9ce52ddbb2630ee80b78865e63dd6d5d0ec
Andrew Dunham <andrew@du.nham.ca>
2019-04-11 00:42:18 -0700

    lego: 1.0.1 -> 2.2.0; upgrade deps

--- pkgs/tools/admin/lego/default.nix
+++ pkgs/tools/admin/lego/default.nix
@@ -2,18 +2,19 @@
 
 buildGoPackage rec {
   name = "lego-${version}";
-  version = "1.0.1";
+  version = "2.2.0";
   rev = "v${version}";
 
   src = fetchFromGitHub {
     inherit rev;
     owner = "xenolf";
     repo = "lego";
-    sha256 = "1l9winhqwid8ac8il303qkhsn0v5h7zhlklviszfi1rjal38ipiz";
+    sha256 = "1bb4kjkbphay63z83a4z2859qrlrx2h7n3rgia17fhy38xp5zzqp";
   };
 
   goPackagePath = "github.com/xenolf/lego";
   goDeps = ./deps.nix;
+  subPackages = [ "cmd/lego" ];
 
   meta = with lib; {
     description = "Let's Encrypt client and ACME library written in Go";
