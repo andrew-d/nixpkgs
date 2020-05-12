bd5c1a9c19f3b7c009813f095341bc19173cfc48
Andrew Dunham <andrew@du.nham.ca>
2020-05-12 00:32:33 -0700

    Update bootstrap tools for ARM
    Combination of #79793 and #82051

--- pkgs/stdenv/linux/bootstrap-tools/default.nix
+++ pkgs/stdenv/linux/bootstrap-tools/default.nix
@@ -1,11 +1,17 @@
 { system, bootstrapFiles }:
 
-derivation {
+let
+  # Conditional to avoid rebuild of Linux
+  unpack-script = if (system == "armv5tel-linux" || system == "armv6l-linux" || system == "armv7l-linux")
+                  then ./scripts/unpack-bootstrap-tools-new.sh
+                  else ./scripts/unpack-bootstrap-tools.sh;
+
+in derivation {
   name = "bootstrap-tools";
 
   builder = bootstrapFiles.busybox;
 
-  args = [ "ash" "-e" ./scripts/unpack-bootstrap-tools.sh ];
+  args = [ "ash" "-e" unpack-script ];
 
   tarball = bootstrapFiles.bootstrapTools;
 
